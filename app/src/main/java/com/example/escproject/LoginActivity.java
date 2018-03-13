803046b47107fcf1f9b28444468fc331ae40b9a3
weian27 <weian_low@mymail.sutd.edu.sg>
2018-03-13 14:41:55 +0800

    added firebase and forgot password

--- app/src/main/java/com/example/escproject/LoginActivity.java
+++ app/src/main/java/com/example/escproject/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.escproject;
 
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -10,30 +11,52 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
     private static final String TAG = "LoginActivity";
+    private FirebaseAuth auth;
 
     TextInputEditText emailText;
     TextInputEditText passwordText;
     Button loginButton;
     TextView signupLink;
+    TextView resetpassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // Get Firebase auth instance
+        auth = FirebaseAuth.getInstance();
+
         emailText = findViewById(R.id.input_email);
         passwordText = findViewById(R.id.input_password);
         loginButton = findViewById(R.id.btn_login);
         signupLink = findViewById(R.id.link_signup);
+        resetpassword = findViewById(R.id.link_resetpassword);
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 login();
             }
+        });
+
+        /* Execute when resetpassword link pressed, goes to the resetpassword page */
+        resetpassword.setOnClickListener(new View.OnClickListener() {
+             @Override
+             public void onClick(View view) {
+                 Intent intent = new Intent(getApplicationContext(), ResetpasswordActivity.class);
+                 startActivity(intent);
+                 finish();
+             }
         });
 
         /* Execute when signup link pressed, goes to the signup page */
@@ -66,25 +89,21 @@ public class LoginActivity extends AppCompatActivity {
         String email = emailText.getText().toString();
         String password = passwordText.getText().toString();
 
-        // TODO: Login authentication
-        boolean isEmailMatch = email.equals("john@mymail.sutd.edu.sg");
-        boolean isPasswordMatch = password.equals("12345678");
-        if (isEmailMatch) {
-            Log.i(TAG, "Email matched");
-            if (isPasswordMatch) {
-                Intent intent = new Intent(getBaseContext(), CourseActivity.class);
-                startActivity(intent);
-                Log.i(TAG, "Password matched");
+        // Login authentication
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (!task.isSuccessful()) {
+                    Toast.makeText(LoginActivity.this, "Authentication failed, check your email and password or sign up", Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Intent intent = new Intent(LoginActivity.this, CourseActivity.class);
+                    startActivity(intent);
+                    Toast.makeText(getApplicationContext(),"Login Successful", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
             }
-            else {
-                Log.i(TAG, "Password invalid");
-                passwordText.setError("Invalid password");
-            }
-        }
-        else {
-            Log.i(TAG, "Email invalid");
-            emailText.setError("Invalid email address");
-        }
+        });
         new android.os.Handler().postDelayed(
                 new Runnable() {
                     public void run() {
